77d850adee1cfea8cee03a81b1be04bfdd541b1c
Florian <alu.florian@gmail.com>
2022-06-30 01:10:25 +0200

    feat: Add StateProvider to wrapRootElement

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,11 +5,13 @@ import '@fontsource/ibm-plex-mono';
 import { WrapRootElementBrowserArgs } from 'gatsby';
 import { Global } from '@emotion/react';
 import globalStyles from './src/globalStyles';
+import StateProvider from './src/components/StateProvider';
 
 export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => {
     return (
-        <>
+        <StateProvider>
             <Global styles={globalStyles} />
-        </>
+            {element}
+        </StateProvider>
     );
 };

9a336e84ddd6e9f0d8d23f69212db4d9f6059937
Florian <alu.florian@gmail.com>
2022-06-30 22:52:18 +0200

    feat: Add profile picture and catch phrase to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
-import Header from '../components/Header';
 import Section from '../components/Section';
 import { Helmet } from 'react-helmet';
 import { css } from '@emotion/react';
+import ProfilePicture from '../components/ProfilePicture';
+import { FlexColumn, FlexRow } from '../components/Flex';
+import styled from '@emotion/styled';
+import Header from '../components/Header';
 
 const Home: FC = () => {
     return (
@@ -11,6 +14,7 @@ const Home: FC = () => {
                 <meta charSet='utf-8' />
                 <title>Accueil - Florian Alù</title>
             </Helmet>
+            <Header />
             <Section
                 backgroundColor='primary'
                 style={css`
@@ -18,10 +22,43 @@ const Home: FC = () => {
                 `}
                 dotted
             >
-                <Header />
+                <HeaderSpace />
+                <FlexRow
+                    grow={1}
+                    style={css`
+                        height: calc(100% - var(--header-height));
+                    `}
+                >
+                    <FlexColumn
+                        basis='50%'
+                        justifyContent='space-evenly'
+                        style={css`
+                            padding: 4rem 0 4rem 2.875rem;
+                        `}
+                    >
+                        <h1>Florian Alù</h1>
+                        <p
+                            css={css`
+                                font-size: 2.25rem;
+                                font-family: 'IBM Plex Sans', sans-serif;
+                            `}
+                        >
+                            Bonjour, je m’appelle Florian.
+                            <br />
+                            Bienvenue sur mon CV/Portfolio.
+                        </p>
+                    </FlexColumn>
+                    <FlexColumn basis='50%' justifyContent='center' alignItems='center'>
+                        <ProfilePicture />
+                    </FlexColumn>
+                </FlexRow>
             </Section>
         </>
     );
 };
+
+const HeaderSpace = styled.div`
+    height: var(--header-height);
+`;
 
 export default Home;

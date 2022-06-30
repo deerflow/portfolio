6fc125c9b86853bee5ef53c06ce0b76d2730c6cb
Florian <alu.florian@gmail.com>
2022-06-30 15:31:38 +0200

    feat: Add meta charset & title tag to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import Header from '../components/Header';
 import Section from '../components/Section';
+import { Helmet } from 'react-helmet';
 
 const Home: FC = () => {
     return (
-        <Section color='primary'>
-            <Header />
-        </Section>
+        <>
+            <Helmet>
+                <meta charSet='utf-8' />
+                <title>Accueil - Florian Alù</title>
+            </Helmet>
+            <Section color='primary'>
+                <Header />
+            </Section>
+        </>
     );
 };
 

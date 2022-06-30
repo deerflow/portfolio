1fa6d41ce89e8fa059ba769d9c12349b4a3968af
Florian <alu.florian@gmail.com>
2022-06-30 14:35:21 +0200

    fix: Update useDarkMode hook to add class theme-transition to body when changing theme

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { StateContext } from '../components/StateProvider';
 
 const MATCH_DARK = '(prefers-color-scheme: dark)';
@@ -7,7 +7,7 @@ const useDarkMode = () => {
     const { theme, setTheme } = useContext(StateContext);
     const [isMediaDark, setIsMediaDark] = useState(window?.matchMedia(MATCH_DARK)?.matches || false);
 
-    console.log({ isMediaDark });
+    const transitionTimeout = useRef<number>(0);
 
     useEffect(() => {
         const matcher = window.matchMedia(MATCH_DARK);
@@ -16,6 +16,29 @@ const useDarkMode = () => {
         return () => matcher.removeEventListener('change', onChange);
     }, [setIsMediaDark]);
 
+    useEffect(() => {
+        if (theme === null) {
+            document.body.classList.remove('light');
+            document.body.classList.remove('dark');
+            localStorage.removeItem('theme');
+        } else {
+            document.body.classList.add('theme-transition');
+            clearTimeout(transitionTimeout.current);
+            changeTheme(theme);
+            transitionTimeout.current = window.setTimeout(
+                () => document.body.classList.remove('theme-transition'),
+                1000
+            );
+        }
+    }, [theme]);
+
     return { theme: theme ?? (isMediaDark ? 'dark' : 'light'), setTheme };
 };
+
+const changeTheme = (theme: 'dark' | 'light') => {
+    document.body.classList.remove(theme === 'dark' ? 'light' : 'dark');
+    document.body.classList.add(theme);
+    localStorage.setItem('theme', theme);
+};
+
 export default useDarkMode;

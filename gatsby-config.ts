ebfbe5e3ce20d154a6b3aff438505a1e0cbdf5ba
Florian <alu.florian@gmail.com>
2022-06-30 15:30:27 +0200

    feat: Configure manifest in gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,47 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `gatsby-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `gatsby-portfolio`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        'gatsby-plugin-emotion',
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'assets',
+                path: './src/assets/',
+            },
+            __key: 'assets',
+        },
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Florian Alù - CV & Portfolio',
+                short_name: 'Florian Alù',
+                start_url: '/',
+                background_color: '#f3e1d1',
+                theme_color: '#f3e1d1',
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: 'standalone',
+                icon: 'src/assets/favicon.svg', // This path is relative to the root of the site.
+                // An optional attribute which provides support for CORS check.
+                // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+                // Any invalid keyword or empty string defaults to `anonymous`
+                crossOrigin: `use-credentials`,
+            },
+        },
+    ],
 };
 
 export default config;

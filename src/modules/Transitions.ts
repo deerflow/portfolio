38aa476e489d65ee47130d8010f0a594bc1fd417
Florian <alu.florian@gmail.com>
2022-07-01 00:50:07 +0200

    feat: Add scrollTransition

---
+++ src/modules/Transitions.ts
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react';
+import settings from '../settings';
+
+type DarkModeCSSProperty = 'color' | 'background-color' | 'border-color' | 'fill';
+
+export const themeTransition = (...cssProperties: DarkModeCSSProperty[]) => {
+    return css`
+        html[data-theme-transition='true'] & {
+            transition: ${cssProperties
+                .map(cssProperty => `${cssProperty} ${settings.themeTransitionDuration}ms`)
+                .join(', ')};
+        }
+    `;
+};
+
+type ScrollCSSProperty = 'width' | 'height' | 'padding' | 'margin';
+
+export const scrollTransition = (...cssProperties: ScrollCSSProperty[]) => {
+    return css`
+        html[data-just-scrolled='true'] & {
+            transition: ${cssProperties
+                .map(cssProperty => `${cssProperty} ${settings.scrollTransitionDuration}ms`)
+                .join(', ')};
+        }
+    `;
+};

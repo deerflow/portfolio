dd090f5bdc5335a98610c8262a14bf7e8eb15e22
Florian <alu.florian@gmail.com>
2022-07-04 12:42:05 +0200

    feat: Add Home, skills and experiences section to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react';
-import Section from '../components/Section';
 import { Helmet } from 'react-helmet';
-import { css } from '@emotion/react';
-import ProfilePicture from '../components/ProfilePicture';
-import { FlexColumn, FlexRow } from '../components/Flex';
 import styled from '@emotion/styled';
 import Header from '../components/Header';
+import SkillsSection from '../components/sections/SkillsSection';
+import HomeSection from '../components/sections/HomeSection';
+import ExperiencesSection from '../components/sections/ExperiencesSection';
 
 const Home: FC = () => {
     return (
@@ -16,60 +15,10 @@ const Home: FC = () => {
             </Helmet>
             <Header />
             <SemiHeaderSpace />
-            <Section
-                backgroundColor='primary'
-                style={css`
-                    height: calc(100vh - (var(--dimension-initial-padding) + var(--dimension-header-height) / 2));
-                    margin-bottom: var(--dimension-initial-padding);
-                `}
-                dotted
-                borders
-            >
-                <FlexRow
-                    grow={1}
-                    style={css`
-                        height: calc(100% - var(--dimension-header-height));
-                    `}
-                >
-                    <FlexColumn
-                        basis='50%'
-                        justifyContent='space-evenly'
-                        style={css`
-                            padding: 4rem 0 4rem 2.875rem;
-                        `}
-                    >
-                        <h1>Florian Alù</h1>
-                        <p
-                            css={css`
-                                font-size: 2.25rem;
-                                font-family: 'IBM Plex Sans', sans-serif;
-                            `}
-                        >
-                            Bonjour, je m’appelle Florian.
-                            <br />
-                            Bienvenue sur mon CV/Portfolio.
-                        </p>
-                    </FlexColumn>
-                    <FlexColumn basis='50%' justifyContent='center' alignItems='center'>
-                        <ProfilePicture />
-                    </FlexColumn>
-                </FlexRow>
-            </Section>
-            <Section
-                backgroundColor='secondary'
-                style={css`
-                    height: 100vh;
-                    border-bottom: none;
+            <HomeSection />
 
-                    position: relative;
-                    html[data-scrolled='true'] & {
-                        bottom: 2px;
-                    }
-                `}
-                borders
-            >
-                <div />
-            </Section>
+            <SkillsSection />
+            <ExperiencesSection />
         </>
     );
 };

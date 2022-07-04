36d423715a54c461bc47cc56e9b82257e21dfbf0
Florian <alu.florian@gmail.com>
2022-07-04 18:11:18 +0200

    feat: Add personality section component

---
+++ src/components/sections/PersonalitySection/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react';
+import Section from '../../Section';
+import { css } from '@emotion/react';
+
+const PersonalitySection: FC = () => {
+    return (
+        <Section
+            dotted
+            backgroundColor='primary'
+            borders='all-but-bottom'
+            padding
+            style={css`
+                font-family: 'IBM Plex Mono', monospace;
+                font-size: 1.25rem;
+            `}
+        >
+            <h2>Personnalité</h2>
+            <p>Ce que j'apporterai à votre entreprise :</p>
+            <ul
+                css={css`
+                    list-style: none;
+                    li::before {
+                        content: '- ';
+                    }
+                `}
+            >
+                <li>
+                    <em>Clarté</em> et <em>vivacité d'esprit</em>
+                </li>
+                <li>
+                    <em>Rigueur</em> et <em>efficacité</em> au travail
+                </li>
+                <li>
+                    <em>Polyvalence</em>, capacité d'<em>adaptation</em>
+                </li>
+                <li>
+                    <em>À l'écoute</em>, bon relationnel, je m'intégrerai facilement dans votre équipe
+                </li>
+                <li>
+                    <em>Créatif</em>, débordant d'<em>idées</em>
+                </li>
+                <li>
+                    <em>Autonome</em>, éternel <em>autodidacte</em>
+                </li>
+            </ul>
+        </Section>
+    );
+};
+
+export default PersonalitySection;

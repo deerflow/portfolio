bdfc819f24cb61476a0abfb8e3032057d1bd4482
Florian <alu.florian@gmail.com>
2022-07-25 19:11:19 +0200

    feat: Add Technologies module to bind technology names with their corresponding icon

---
+++ src/modules/Technologies.ts
@@ -0,0 +1,20 @@
+import reactIcon from '../assets/svg/react.svg';
+import nodeIcon from '../assets/svg/nodejs.svg';
+import typeScriptIcon from '../assets/svg/typescript.svg';
+import nextIcon from '../assets/svg/nextjs.svg';
+import expoIcon from '../assets/svg/expo.svg';
+import goIcon from '../assets/svg/go.svg';
+import meteorIcon from '../assets/svg/meteor.svg';
+
+const Technologies = {
+    React: reactIcon,
+    'React Native': reactIcon,
+    'Node.js': nodeIcon,
+    TypeScript: typeScriptIcon,
+    Go: goIcon,
+    'Next.js': nextIcon,
+    Meteor: meteorIcon,
+    Expo: expoIcon,
+};
+
+export default Technologies;

bee9db9693135212e61de16ebc82cebab5859eea
Florian <alu.florian@gmail.com>
2022-07-25 19:11:59 +0200

    feat: Add content/PROJECTS to list all project

---
+++ src/content/PROJECTS.ts
@@ -0,0 +1,12 @@
+import { Project } from '../types/api';
+
+const PROJECTS: Project[] = [
+    {
+        title: 'Application hybride iOS, Android et PWA',
+        organization: { name: 'Campus Skills', href: 'https://campus-skills.com' },
+        description: '',
+        technologies: ['React Native', 'Expo', 'TypeScript'],
+    },
+];
+
+export default PROJECTS;
